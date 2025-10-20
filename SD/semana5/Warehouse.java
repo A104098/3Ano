75e7dbae7d8f19c6ff9f5ccaf162eccbdbb4f91b
A104098_ <a104098@alunos.uminho.pt>
2025-10-20 10:43:50 +0100

    aula

--- SD/semana5/Warehouse.java
+++ SD/semana5/Warehouse.java
@@ -1,9 +1,15 @@
 import java.util.*;
+import java.util.concurrent.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Warehouse {
     private Map<String, Product> map =  new HashMap<String, Product>();
+    lock lock = new ReentrantLock();
 
-    private class Product { int quantity = 0; }
+    private class Product { 
+        int quantity = 0; 
+        Condition c = l.reCondition(); // cada produto tem a sua propria variavel de condicao}
 
     private Product get(String item) { //adição hashmap caso a chave nao exista
         Product p = map.get(item);
@@ -14,15 +20,30 @@ class Warehouse {
     }
 
     public void supply(String item, int quantity) {
+        l.lock();
+        try {
         Product p = get(item); //
         p.quantity += quantity;
+        p.c.signalAll(); // acorda threads que estao a espera deste produto
+        } finally {
+            l.unlock();
+        }
     }
 
     // Errado se faltar algum produto...
     public void consume(Set<String> items) {
+        l.lock();
+        try {
         for (String s : items)
-            get(s).quantity--;
-    }
+            Product p = get(s);
+            while (p.quantity == 0)
+                p.c.await(); // espera pelo produto especifico
+                get(s).quantity--;
 
+        } finally {
+    }
+        l.unlock();
+        }
 }
 // todas as variaveis de condição sao criadas associadas a um lock apenas (ver video yt de "conditions and lock")
+

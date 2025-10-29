3df613371f9187f52c1e1d56c25604ed3980e4b9
A104098_ <a104098@alunos.uminho.pt>
2025-10-29 16:55:16 +0000

    aula29/10 jogo do galo

--- IPM/Semana5/todo-app-alunos/todo.js
+++ IPM/Semana5/todo-app-alunos/todo.js
@@ -8,9 +8,9 @@ const todos = [
 /* Ex. 1: Add a event listener that triggers when the DOM is loaded here */
 document.addEventListener('DOMContentLoaded', () => {
   const dateElement = document.getElementById('list-date');
-  const today = new Date(); //dia de hoje
-  dateElement.textContent = today.toLocaleDateString();
-  
+  const today = new Date().toLocaleDateString('pt-PT'); //dia de hoje
+  dateElement.textContent = today;
+  renderTodoList();
 });
 
 
@@ -24,6 +24,25 @@ function removeAllChildNodes(parent) {
 
 // render todo array here
 function renderTodoList() {
+
+  const todoListElement = document.getElementById('todo-list');
+
+  todos.forEach(todo => {
+    const todoItem = document.createElement('li');
+    const todoText = document.createElement('p');
+    const todoButton = document.createElement('button');
+
+    todoText.textContent = todo;
+    todoButton.textContent = 'Delete';
+
+    todoItem.classList.add('todo-list-item');
+    todoItem.appendChild(todoText);
+
+    todoItem.appendChild(todoButton);
+
+    todoListElement.appendChild(todoItem);
+
+})
   
 }
 
@@ -31,7 +50,21 @@ function renderTodoList() {
 
 /* Ex. 3: Add a event listener to element 'todo-form'*/
 document.getElementById('todo-form').addEventListener('submit', event => {
-  
+  event.preventDefault();//nao faz refresh da pagina
+  const taskInput = document.getElementById('task-input');
+  const todoValue = taskInput.value;
+  taskInput.value = '';//limpar o input
+
+  if(todoValue === '') return;
+
+  //se tentarmos adicionar um todo vazio, nada acontece
+  //dar trigger a alerta 
+  if(todos.includes(todoValue)) {
+    alert('Erro! ' + todoValue +  ' já existe.');
+    return;
+  }
+  todos.push(todoValue);
+  renderTodoList();  
 });
 
 

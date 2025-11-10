5cc07d129d91c7169338750d3d49944e3691ff09
A104098_ <a104098@alunos.uminho.pt>
2025-11-10 19:40:29 +0000

    restp da aula 10/11

--- IPM/Semana 9/4inarow_alunos/src/components/Navbar.vue
+++ IPM/Semana 9/4inarow_alunos/src/components/Navbar.vue
@@ -2,10 +2,10 @@
 import { ResultadoJogada } from '@/models/resultadoJogada';
 import (ResultadoJogada)
 export default {
-  props: ['jogadorAJogar', 'vencedor']
-  computed:{
-    texto(){
-      if(this.vencedor == ResultadoJogada.VERMELHO)
+  props: ['jogadorAJogar', 'vencedor'],
+  computed: {
+    texto() {
+      if (this.vencedor == ResultadoJogada.VERMELHO)
         return "Vencedor: Vermelho";
       else if(this.vencedor == ResultadoJogada.AMARELO)
         return "Vencedor: Amarelo";
@@ -15,6 +15,18 @@ export default {
 </script>
 
 <template>
+  <div class="navbar">
+    <h3>4 em Linha</h3>
+    <div 
+    class="button" 
+    v-if="this.vencedor === null" 
+    :class="{red: this.jogadorAJogar, yellow: !this.jogadorAJogar}">
+      <p>Jogador a jogar: {{ jogadorAJogar ? 'Vermelho' : 'Amarelo' }}</p>
+    </div>
+    <div v-else class="button" disabled>
+      <p>{{ texto }}</p>
+    </div>
+  </div>
 
 </template>
 

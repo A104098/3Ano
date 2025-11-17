ff2577d7ef4e7dae46db6ddb861275a2abd9b3d6
A104098 <A104098@alunos.uminho.pt>
2025-11-17 10:43:09 +0000

    aula

--- SD/Semana9/g8/FramedConnection.java
+++ SD/Semana9/g8/FramedConnection.java
@@ -1,69 +1,52 @@
 package g8;
 
+import java.awt.Frame;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 
 public class FramedConnection implements AutoCloseable {
-    private final Socket socket;
-    private final DataInputStream in;
-    private final DataOutputStream out;
-    private final Object inLock = new Object();
-    private final Object outLock = new Object();
+    DataInputStream dis;
+    DataOutputStream dos;
+    ReentrantLock ls = new ReentrantLock();
+    ReentrantLock lr = new ReentrantLock();
+    Socket socket;
 
     public FramedConnection(Socket socket) throws IOException {
+        this.dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        this.dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
         this.socket = socket;
-        this.in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-        this.out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
     }
 
     /**
      * Send a message (byte array). First writes the length (int) then the bytes.
      */
-    public void send(byte[] data) throws IOException {
-        if (data == null) throw new NullPointerException("data");
-        synchronized (outLock) {
-            out.writeInt(data.length);
-            out.write(data);
-            out.flush();
-        }
+    public void send(Frame frame) throws IOException {
+        send(frame.tag, frame.data);
     }
 
     /**
      * Receive next message: reads int length then reads fully the byte array.
      */
-    public byte[] receive() throws IOException {
-        synchronized (inLock) {
-            int len = in.readInt();
-            if (len < 0) throw new IOException("Negative frame length: " + len);
-            byte[] data = new byte[len];
-            in.readFully(data);
-            return data;
+    public Frame receive() throws IOException {
+        lr.lock();
+        try {
+            int len = dis.readInt();
+            int tag = dis.readInt();
+            byte[] ba = new byte[len - 4];
+            dis.readFully(ba);
+            return new Frame(tag, ba);
+        } finally {
+            lr.unlock();
         }
     }
-
-    @Override
     public void close() throws IOException {
-        IOException ex = null;
-        try {
-            in.close();
-        } catch (IOException e) {
-            ex = e;
-        }
-        try {
-            out.close();
-        } catch (IOException e) {
-            if (ex == null) ex = e;
-        }
-        try {
-            socket.close();
-        } catch (IOException e) {
-            if (ex == null) ex = e;
-        }
-        if (ex != null) throw ex;
+        socket.close();
     }
 }
